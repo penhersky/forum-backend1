aea1bee8f8a4c4fe013e8fff07a8d6f451702354
Odavinh <46005885+Odavinh@users.noreply.github.com>
2019-09-20 09:55:31 +0300

    Update user.js

--- src/resolver/types/user.js
+++ src/resolver/types/user.js
@@ -4,8 +4,7 @@ import models from "../../database/models/models";
 
 export const User = {
   image: async parent => {
-    const userImage = await models.userImageModel.findOne({owner: parent.id});
-    return await readFileSync(userImage.image);
+    return await models.userImageModel.findOne({owner: parent.id});
   },
   additionalInfo: async parent => {
     return await models.fullUserModel.findOne({owner: parent.id});
